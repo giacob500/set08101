05e66c7970bc0840cb18674fa1b4d82d1e8e61af
GIacomo Lorenzi <giacob500@gmail.com>
2022-04-20 18:25:53 +0200

    Added questions

---
+++ cwk/js/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Name the person who replaced Ozzy Osbourne as Black Sabbath's lead singer?",
+        answer: "Ronnie James Dio",
+        options: [
+            "Ronnie James Dio",
+            "Judas Priest",
+            "Dokken",
+            "Whitesnake"
+        ]
+    },
+    {
+        numb: 2,
+        question: "To consider a band as a Big Band what is the minimum number of musicians to be needed?",
+        answer: "11",
+        options: [
+            "11",
+            "21",
+            "12",
+            "10"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Bjork was lead singer of what Icelandic band before pursuing a solo career?",
+        answer: "The Sugarcubes",
+        options: [
+            "Kukl",
+            "The Sugarcubes",
+            "Cocteau Twins",
+            "The Elgar sisters"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In an Orchestra, which is the largest brass section instrument?",
+        answer: "Tuba",
+        options: [
+            "Trumpet",
+            "Tenor",
+            "French horns",
+            "Tuba"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Name the singer who released the album Alf?",
+        answer: "Alison Moyet",
+        options: [
+            "Alison Moyet",
+            "Aretha Louise Franklin",
+            "Michael Joseph Jackson",
+            "Christopher Maurice"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Name the singer who released the album, Here, My Dear?",
+        answer: "Marvin Gaye",
+        options: [
+            "Frankie Gaye",
+            "Bruno Mars",
+            "Elton John",
+            "Marvin Gaye"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Puff the Magic Dragon lives in which place",
+        answer: "Hanalei",
+        options: [
+            "Neverland",
+            "Asgard",
+            "Bristol",
+            "Hanalei"
+        ]
+    },
+    {
+        numb: 8,
+        question: "A tribute to David Bowie’s ex-wife was given with a song by The Rolling stones, name the song?",
+        answer: "Angie",
+        options: [
+            "Wild Horses",
+            "Gimme Shelter",
+            "Angie",
+            "Satisfaction"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Name the blues great who was born Ellas Otha Bates?",
+        answer: "Bo Diddley",
+        options: [
+            "Little Richard",
+            "Muddy Waters",
+            "Bo Diddley",
+            "Chuck Berry"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Name the band having its name after a scientist from the movie Barbarella?",
+        answer: "Duran Duran",
+        options: [
+            "Duran Duran",
+            "A Ha",
+            "Nile Rodgers",
+            "Tears for fears"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Name the orchestral instrument that can play high note?",
+        answer: "Violin",
+        options: [
+            "Viola",
+            "Piccolo",
+            "Violin",
+            "Cellos"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Name the all-time bestselling movie soundtrack?",
+        answer: "The Bodyguard: Original Soundtrack Album",
+        options: [
+            "I have nothing",
+            "My love is your love",
+            "Waiting to exhale",
+            "The Bodyguard: Original Soundtrack Album"
+        ]
+    },
+    {
+        numb: 13,
+        question: "The most played song on American radio during the 20th century was?",
+        answer: "You’ve Lost That Loving Feeling",
+        options: [
+            "You’ve Lost That Loving Feeling",
+            "Just Once in My Life",
+            "Danger Zone",
+            "Soul and Inspiration"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Name the musical director of the New York Philharmonic from 1958 to 1970?",
+        answer: "Jaap Van zweden",
+        options: [
+            "Jaap Van zweden",
+            "Alan Gilbert",
+            "Leonard Bernstein",
+            "Kurt Masur"
+        ]
+    },
+    {
+        numb: 15,
+        question: "The Clawhammer is a playing style associated with an instrument, what is it?",
+        answer: "Banjo",
+        options: [
+            "Ukulele",
+            "Banjo",
+            "Mandolin",
+            "Fiddle"
+        ]
+    },
+    {
+        numb: 16,
+        question: "The 2001, Grammy Award for Best New Artist was won by whom?",
+        answer: "Amy Winehouse",
+        options: [
+            "Sam Smith",
+            "Amy Winehouse",
+            "Meghan Trainor",
+            "Alessia Cara"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Who recorded 'Groove is in the Heart'?",
+        answer: "Deee-Lite",
+        options: [
+            "Q-Tip",
+            "Deee-Lite",
+            "Technotronic",
+            "The B-52’s"
+        ]
+    },
+    {
+        numb: 18,
+        question: "The lead singer of Evanescence is?",
+        answer: "Amy Lee",
+        options: [
+            "Jen Majura",
+            "Lzzy Hale",
+            "Ben Moody",
+            "Amy Lee"
+        ]
+    },
+    {
+        numb: 19,
+        question: "Among the following, name the musical group that had the 1967 Top 10 single, 'I Think We’re Alone Now'?",
+        answer: "Tommy James and the Shondells",
+        options: [
+            "The Rascals",
+            "The Troggs",
+            "Tommy James and the Shondells",
+            "The Turtles"
+        ]
+    },
+    {
+        numb: 20,
+        question: "The musical composition “Rhapsody in Blue was written by whom?",
+        answer: "George Gershwin",
+        options: [
+            "Leonard Bernstein",
+            "Cole Porter",
+            "Claude Debussy",
+            "George Gershwin"
+        ]
+    },
+];
