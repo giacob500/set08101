266b5f18feba800b679122378a23d5d168a99143
GIacomo Lorenzi <giacob500@gmail.com>
2022-04-24 23:38:52 +0100

    show icons next to given and correct answer

--- cwk/js/script.js
+++ cwk/js/script.js
@@ -4,6 +4,7 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
 
 // if startQuiz button is clicked
 start_btn.onclick = ()=>{
@@ -43,7 +44,6 @@ next_btn.onclick = ()=>{
 // getting questions and options from array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
     let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
@@ -60,15 +60,36 @@ function showQuestions(index){
     }
 }
 
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check-circle"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
 function optionSelected(answer){
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
     if (userAns == correctAns){
         answer.classList.add("correct");
         console.log("Answer is correct");
+        answer.insertAdjacentHTML("beforeend", tickIconTag);
     } else {
         answer.classList.add("incorrect");
         console.log("Answer is Wrong");
+        answer.insertAdjacentHTML("beforeend", crossIconTag);
+
+        // if the answer is wrong automatically show correct answer
+        for (let i = 0; i < allOptions; i++) {
+            if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+            }       
+        }
+    }
+
+    // once user selected disabled all options
+    for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+        
     }
 }
 
