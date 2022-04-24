73ce4345bd03896fafd76779946830dd37809a6b
GIacomo Lorenzi <giacob500@gmail.com>
2022-04-24 18:53:36 +0100

    implemented continue button

--- cwk/js/script.js
+++ cwk/js/script.js
@@ -3,17 +3,29 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
-// if startQuiz button clicked
+// if startQuiz button is clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
 }
-// if exitQuiz button clicked
+// if exitQuiz button is clicked
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
 }
-// if continueQuiz button clicked
+
+// if continueQuiz button is clicked
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuestions(0); //calling showQestions function
+}
+
+let que_count = 0;
+
+// getting questions and options from array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>' + questions[index].question + '</span>';
+    que_text.innerHTML = que_tag;
 }
