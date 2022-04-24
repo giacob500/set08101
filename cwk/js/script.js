75d2b5191996dd4a9c0fc2e689a89584b2d8e2bd
GIacomo Lorenzi <giacob500@gmail.com>
2022-04-24 19:21:43 +0100

    'next question button' implemented

--- cwk/js/script.js
+++ cwk/js/script.js
@@ -8,6 +8,7 @@ const quiz_box = document.querySelector(".quiz_box");
 // if startQuiz button is clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
+    next_btn.classList.add("show"); // testing
 }
 // if exitQuiz button is clicked
 exit_btn.onclick = ()=>{
@@ -22,10 +23,28 @@ continue_btn.onclick = ()=>{
 }
 
 let que_count = 0;
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If next button is clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        showQuestions(que_count);
+    } else {
+        console.log("Questions completed");
+    }
+    
+}
 
 // getting questions and options from array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text");
-    let que_tag = '<span>' + questions[index].question + '</span>';
-    que_text.innerHTML = que_tag;
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
 }
